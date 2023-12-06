0ef38c7e1d84d4502021c009965542474d33aaa3
lupamo <arnoldlupamo@gmail.com>
2023-12-06 21:12:09 +0300

    dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for l, s in a_dictionary.items():
-        print("{}: {}".format(l, s))
+    dic_comp = ["{}: {}".format(
+        key, value) for key, value in a_dictionary.items()]
+    print("\n".join(dic_comp))
