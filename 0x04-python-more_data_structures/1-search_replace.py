244038a186d45b31b95e5e4e8ef86e4f6b4414c8
lupamo <arnoldlupamo@gmail.com>
2023-12-06 19:55:11 +0300

    comper

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for index, item in enumerate(my_list):
-        if item == search:
-            my_list[index] = replace
-    return my_list
+    return [replace if item == search else item for item in my_list]
