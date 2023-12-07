ec5134c63a690de78eb2af7121fef6ba589b7e72
lupamo <arnoldlupamo@gmail.com>
2023-12-07 09:40:28 +0300

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     output = 0
-    i = 0
-    roman_dics = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dics = {'I': 1,
+                  'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     rom_digit = roman_string
-    if len(rom_digit) == 0 != str or type(rom_digit):
+    if not roman_string or not isinstance(roman_string, str):
         return 0
-    for i in range(i, len(rom_digit)):
-        if i < len(rom_digit) - 1 and roman_dics[rom_digit[i]] < roman_dics[rom_digit[i + 1]]:
+    for i in range(len(rom_digit)):
+        if i < len(rom_digit) - 1 and \
+              roman_dics[rom_digit[i]] < roman_dics[rom_digit[i + 1]]:
             output -= roman_dics[rom_digit[i]]
         else:
             output += roman_dics[rom_digit[i]]
