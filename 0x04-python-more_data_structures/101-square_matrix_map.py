659d6b3b7b9c02d00e5d3285cf405e232dbcd9ae
lupamo <arnoldlupamo@gmail.com>
2023-12-08 10:12:13 +0300

    mul matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    mul_matrix = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
-    return mul_matrix
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
