bb61db1c2877903d8938f8431c5bb4a5e4b145f8
lupamo <arnoldlupamo@gmail.com>
2023-12-08 09:34:54 +0300

    return value

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list == 0:
+        return 0
+
     mutiplied = sum(x * y for x, y in my_list)
     added = sum(j[1] for j in my_list)
     average_w = mutiplied / added
