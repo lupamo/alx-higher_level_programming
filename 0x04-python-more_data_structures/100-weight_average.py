fcca9f5fcad38653c13770b1d52d323ef77a85df
lupamo <arnoldlupamo@gmail.com>
2023-12-08 09:47:18 +0300

    sum value addes

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == 0:
-        return 0
-
     sum_multiple = 0
     sum_added = 0
 
-    for x, y in my_list:
-        sum_multiple += x * y
+    if my_list == 0:
+        return 0
+    else:
+        for x, y in my_list:
+            sum_multiple += x * y
 
-    for j in my_list:
-        sum_added += j[1]
+        for j in my_list:
+            sum_added += j[1]
     average_w = sum_multiple / sum_added
     return average_w
