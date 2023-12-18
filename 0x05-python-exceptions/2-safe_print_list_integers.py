996e796b5a8bd72e4e01a6f1cb035fdb06afb30e
lupamo <arnoldlupamo@gmail.com>
2023-12-18 22:39:06 +0300

    isdigit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     idx = 0
     for j in range(x):
         try:
-            print("{}".format(my_list[j], end=""), end="")
+            print("{:d}".format(my_list[j], end=""), end="")
             idx += 1
         except (ValueError, TypeError):
             continue
