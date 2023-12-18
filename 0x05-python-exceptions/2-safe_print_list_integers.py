b192e4c919aceb26960fa5119374c8d488b4b431
lupamo <arnoldlupamo@gmail.com>
2023-12-18 22:36:30 +0300

    value and type

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    idx = 0
+    for j in range(x):
+        try:
+            print("{}".format(my_list[j], end=""), end="")
+            idx += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return idx
