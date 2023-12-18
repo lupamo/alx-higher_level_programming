d6229d6c18cb1a2a2b7b0b0e9cd41e92fe9e6d49
lupamo <arnoldlupamo@gmail.com>
2023-12-18 22:06:43 +0300

    Attributeerror

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value.isdigit():
+        if isinstance(value, int):
             print("{:d}".format(value))
             return True
         else:
