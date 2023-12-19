660e6b316cda912c268c02e6330b3977158bdfa2
lupamo <arnoldlupamo@gmail.com>
2023-12-19 21:21:20 +0300

    private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""ä class with private instance variable"""
+
+
+class Square:
+    """"self initializing"""
+
+    def __init__(self, size):
+        pass
+
+
+""""closed the square class size"""
