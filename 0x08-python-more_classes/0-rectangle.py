648ad2643c5e48d43b63a3538c53f7dc4f883083
lupamo <arnoldlupamo@gmail.com>
2024-01-04 20:15:10 +0300

    rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A rectangle class which won't get executed"""
+
+
+class Rectangle:
+    """the pass ignores the executing requirement"""
+
+    pass
+
+
+"""closed function no execution"""
