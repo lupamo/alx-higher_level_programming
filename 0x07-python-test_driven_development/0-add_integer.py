a3275e1e9e37b8bff055c334e4de0199a7f94958
lupamo <arnoldlupamo@gmail.com>
2024-01-07 21:36:37 +0300

    cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,13 +8,17 @@
 
 def add_integer(a, b=98):
 
-    """testing a and b"""
+    """testing a and b
+    if a or b are not an int or float raise error
+    """
 
     if not isinstance(a, (int, float)):
+
         """Raising an Error when a is not integer or float"""
 
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
+
         """Raising an Error whan b is not an int or float"""
 
         raise TypeError("b must be an integer")
