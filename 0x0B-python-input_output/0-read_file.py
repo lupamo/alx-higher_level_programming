c5f3d46619d8d7ad5b7ffc02ec17e0e22cd96c69
lupamo <arnoldlupamo@gmail.com>
2024-01-09 21:41:26 +0300

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,9 +8,6 @@ Returns, printed file
 def read_file(filename=""):
     """Reads a file"""
 
-    try:
-        with open(f'{filename}', 'r', encoding="utf-8") as f:
-            file_read = f.read()
-            print(file_read)
-    except FileNotFoundError:
-        print(f"{filename} doesn't exist")
+    with open(f'{filename}', 'r', encoding="utf-8") as f:
+        for line in f:
+            print(line, end="\n")
