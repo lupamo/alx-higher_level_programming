f2d08b0b1ee05c5690f3bddef92741020c663989
lupamo <arnoldlupamo@gmail.com>
2024-01-09 08:12:16 +0300

    exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""
+A class BaseGeometry
+with an area instance method
+"""
+
+
+class BaseGeometry:
+    """pass"""
+
+    def __init__(self):
+        pass
+
+    """
+    area instance method that raises an exception
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
