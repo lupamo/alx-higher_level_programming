a9af30a27e37b211f1a43c7b6d2e332c7037185f
lupamo <arnoldlupamo@gmail.com>
2024-01-10 01:29:55 +0300

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+a function that returns a list of ints
+representing pascal triangle
+Technical interview
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    p_triangle = [[1]]
+
+    while len(p_triangle) != n:
+        trio = p_triangle[- 1]
+        temp = [1]
+        for j in range(len(trio) - 1):
+            temp.append(trio[j] + trio[j + 1])
+        temp.append(1)
+        p_triangle.append(temp)
+    return p_triangle
