c4f107c16e2c88a1657a77f8817fc0ada9794c64
lupamo <arnoldlupamo@gmail.com>
2024-01-09 22:13:11 +0300

    f

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,5 +8,5 @@ to a text file
 def append_write(filename="", text=""):
     """appending a text on file"""
 
-    with open(f'f{filename}', 'a', encoding="UTF8") as f:
+    with open(f'{filename}', 'a', encoding="UTF8") as f:
         return f.write(text)
