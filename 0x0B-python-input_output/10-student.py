14c87e45394f6b3e11783674ac9796ced632150c
lupamo <arnoldlupamo@gmail.com>
2024-01-10 00:44:12 +0300

    student dict

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A class that defines a student
+"""
+
+
+class Student:
+    """initializing the class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        """turns a dictionary of student instance"""
+        new_dict = {}
+        new_dict = self.__dict__
+        if attrs is None:
+            return new_dict
+        for key, value in new_dict.items():
+            if key in attrs:
+                new_dict[key] = value
+            return new_dict
