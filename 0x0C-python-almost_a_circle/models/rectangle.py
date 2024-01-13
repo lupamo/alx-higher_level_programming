2af2bb5cab1db977f04521bf636775c49c898183
lupamo <arnoldlupamo@gmail.com>
2024-01-13 05:17:04 +0300

    setter

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,7 @@ Rectengle class which inherits from Base
 
 from models.base import Base
 
+
 class Rectangle(Base):
     """Base inherited by Rectangle"""
 
@@ -30,30 +31,70 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Allows us to define width which is called
+        when getting, setting or deleting the value of the property
+        """
+
         return self.__width
+
     @width.setter
     def width(self, value):
-        if value >= 0:
-            self.__width = value
+        """method that modifies the width attribute"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
 
     @property
     def height(self):
+        """Allows us to define height which is called
+        when getting, setting or deleting the value of the property
+        """
         return self.__height
+
     @height.setter
     def height(self, value):
-        if value >= 0:
-            self.__height = value
+        """method that modifies the height attribute"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
 
     @property
     def x(self):
+        """it allows x access special methods(getter, setter and deleter)"""
+
         return self.__x
+
     @x.setter
     def x(self, value):
+        """modifies our private x attribute
+        value: the value which x attribute is to be set to
+        """
+        if not value < 0:
+            raise TypeError("x must be >= 0")
+
         self.__x = value
 
     @property
     def y(self):
+        """allows y to access special methods
+            getter, setter and deleter methods)"""
+
         return self.__y
+
     @y.setter
     def y(self, value):
+        """modifies our private x attribute
+        value: the value which x attribute is to be set to
+        """
+        if value < 0:
+            raise TypeError("y must be >= 0")
         self.__y = value
