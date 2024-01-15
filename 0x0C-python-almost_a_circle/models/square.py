af4e6a8e798346072c71de5098f4e43bf278630f
lupamo <arnoldlupamo@gmail.com>
2024-01-15 17:02:52 +0300

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class that inherits from Rectangle"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Setting width and height to be equal to value hence size"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.width = size
+        self.height = size
