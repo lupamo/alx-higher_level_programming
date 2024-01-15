42a51c0ec4cd89fa5d1354576f0b13b5a61cfe21
lupamo <arnoldlupamo@gmail.com>
2024-01-15 17:34:39 +0300

    Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,9 +5,24 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Setting width and height to be equal to value hence size"""
+    """initializing Square with Rectangle's instances"""
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.width = size
-        self.height = size
+
+    """Setting width to be equal to value hence size"""
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """method that modifies the width and height attribute"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
