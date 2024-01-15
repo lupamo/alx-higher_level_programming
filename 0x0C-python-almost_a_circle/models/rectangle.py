18abbaf8efb7d29eda709943ef7924bc90c464b8
lupamo <arnoldlupamo@gmail.com>
2024-01-15 12:44:08 +0300

    Update #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -124,7 +124,7 @@ class Rectangle(Base):
             self.id, self.__x, self.__y, self.width, self.height)
 
     """Updating rectangle with args"""
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         if len(args) >= 1:
             self.id = args[0]
         if len(args) >= 2:
@@ -135,3 +135,6 @@ class Rectangle(Base):
             self.x = args[3]
         if len(args) >= 5:
             self.y = args[4]
+        """updating attributes using keyword arguments"""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
