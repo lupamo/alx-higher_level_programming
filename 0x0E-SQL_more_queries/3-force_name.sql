49483a27c412393ca9276c56803f8429a78e683f
lupamo <arnoldlupamo@gmail.com>
2024-02-14 11:40:28 -0800

    school

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- a script that creates the table force_name on MySQL server.
-CREATE TABLE IF NOT EXITS force_name (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
 
