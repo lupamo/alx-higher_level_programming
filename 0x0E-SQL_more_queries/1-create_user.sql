95e211899703dc8c3281e75a633d5820d081b37a
lupamo <arnoldlupamo@gmail.com>
2024-02-14 10:05:55 -0800

    add user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- script that creates user and gives him previleges
-IF 'user_0d_1' NOT EXIST CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+IF 'user_0d_1' NOT EXISTS
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
