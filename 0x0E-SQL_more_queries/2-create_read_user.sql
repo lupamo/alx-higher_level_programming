36069c4d5633b08a7fbfcdc778d45ce973b9f603
lupamo <arnoldlupamo@gmail.com>
2024-02-14 11:11:24 -0800

    local host

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- a script that creates a database and user
+-- user is given only select privilage
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON htbn_0d_2.* TO user_0d_2@localhost;
