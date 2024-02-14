4ac532259cdd191d4ad422be2e7b29b275e42274
lupamo <arnoldlupamo@gmail.com>
2024-02-14 11:35:51 +0300

    underscore

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0
 SELECT score COUNT(*) AS number FROM second_table
-GROUP_BY score ORDER BY number DESC;
+GROUP BY score ORDER BY number DESC;
 
