0f6f7dd65712806b32966150bbbfa49517007108
lupamo <arnoldlupamo@gmail.com>
2024-02-15 11:01:01 -0800

    in

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- a script that creates the database hbtn_0d_usa and the table state
-CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
 
