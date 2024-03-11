539a68a08150b50292816983435ba52a8e8b7e8b
lupamo <arnoldlupamo@gmail.com>
2024-03-11 15:38:30 -0700

    gate

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,6 @@ const nums = argv.slice(2).map(Number).filter(num => Number.isInteger(num));
 if (nums.length < 2) {
   console.log('0');
 } else {
-  const sortedNums = numbers.sort((a, b) => b - a);
+  const sortedNums = nums.sort((a, b) => b - a);
   console.log(`${sortedNums[1]}`);
 }
