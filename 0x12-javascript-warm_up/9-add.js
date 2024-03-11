4692bbf527caefbbcd7cabd78b99feed926c2528
lupamo <arnoldlupamo@gmail.com>
2024-03-11 14:31:49 -0700

    adding args

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 if (argv.length === 4) {
-  console.log(argv[2] + argv[3]);
+  console.log(parseInt(argv[2]) + parseInt(argv[3]));
 } else if (argv.length === 3) {
   console.log('NaN');
 } else {
