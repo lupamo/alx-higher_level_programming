d88d5aa4fc6140600f7ec0e78b9694daaae3953f
lupamo <arnoldlupamo@gmail.com>
2024-03-11 12:03:36 -0700

    spaces

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,13 +2,16 @@
 const { argv } = require('node:process');
 
 argv.forEach((val, index) => {
-  for (let idx = 0; idx <= index; idx++ ){
-    if (idx === 1) {
-       console.log('No argument');
-    } else if (idx === 2) {
+  let argCount = 0;
+
+  for (let idx = 0; idx <= index; idx++) {
+    argCount += idx;
+    if (argCount === 1) {
+      console.log('No argument');
+    } else if (argCount === 2) {
       console.log('Argument found');
     } else {
       console.log('Arguments found');
     }
   }
-})
+});
