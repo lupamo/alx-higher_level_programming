f9c6963e561031aa758bb406a281da382e57c7f0
lupamo <arnoldlupamo@gmail.com>
2024-03-11 13:11:14 -0700

    semi concar

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const { argv } = required('node:process');
-if (argv.length === 3) {
-  console.log(`${argv[1]} is ${argv[2]}`);
-} else if (argv.length === 2) {
-  console.log(`${argv[1]} is undefined`);
+const { argv } = require('node:process');
+if (argv.length === 4) {
+  console.log(`${argv[2]} is ${argv[3]}`);
+} else if (argv.length === 3) {
+  console.log(`${argv[2]} is undefined`);
 } else {
   console.log('undefined is undefined');
 }
