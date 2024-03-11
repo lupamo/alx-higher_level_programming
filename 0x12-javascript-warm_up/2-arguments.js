b2f32b6dfdc53eaf2457dabd2f96d22164485c85
lupamo <arnoldlupamo@gmail.com>
2024-03-11 20:31:15 +0300

    argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (argv.length === 0){
+  console.log('No argument');
+} else if (argv.length === 1){
+  console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
