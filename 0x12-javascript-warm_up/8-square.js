4d27760161b558a4207733c1c91c5829751f8779
lupamo <arnoldlupamo@gmail.com>
2024-03-11 14:23:55 -0700

    naun

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length === 3) {
+if (argv.length === 3 || isNaN(argv[2])) {
   for (let i = 0; i < argv[2]; i++) {
     let row = '';
     for (let j = 0; j < argv[2]; j++) {
