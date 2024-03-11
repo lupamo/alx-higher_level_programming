d22d02a2860338eee5d78ea7f7795b12f48c7038
lupamo <arnoldlupamo@gmail.com>
2024-03-11 13:31:13 -0700

    numbers

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (!isNaN(argv[1])) {
+if (!isNaN(argv[2])) {
   console.log(`My number: ${parseInt(argv[2], 10)}`);
 } else {
 	console.log('Not a number');
