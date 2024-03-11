959d87cac5d7be0bbb211bebc77c87f585889e3e
lupamo <arnoldlupamo@gmail.com>
2024-03-11 22:09:07 +0300

    no module

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,16 +2,12 @@
 const { argv } = require('node:process');
 
 argv.forEach((val, index) => {
-  let argCount = 0;
 
-  for (let idx = 0; idx <= index; idx++) {
-    argCount += idx;
-    if (argCount === 1) {
-      console.log('No argument');
-    } else if (argCount === 2) {
-      console.log('Argument found');
-    } else {
-      console.log('Arguments found');
-    }
+  if (argv.length == 1) {
+    console.log('No argument');
+  } else if (argv.length == 2) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
   }
 });
