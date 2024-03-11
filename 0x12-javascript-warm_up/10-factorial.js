96957b05f7acc14fb7d48b7e83270628e2dd6d53
lupamo <arnoldlupamo@gmail.com>
2024-03-11 15:13:59 -0700

    factory

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-function factorial(n) {
+function factorial (n) {
   if (n === 0 || n === 1) {
     console.log(1);
   } else {
-    let fact = 1;
-    for (let i = 2; i <= argv[2]; i++) {
-        fact *= i;
+    let fact = 0;
+    for (let i = 2; i <= n; i++) {
+      fact *= i;
     }
     console.log(fact);
-    }
+  }
 }
 console.log(factorial(parseInt(argv[2])));
