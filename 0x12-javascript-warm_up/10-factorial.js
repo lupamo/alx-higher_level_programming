97e18325048073da4c44c6d40d8461b74d981b36
lupamo <arnoldlupamo@gmail.com>
2024-03-11 14:54:39 -0700

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,10 +4,12 @@ if (!isNaN(argv[2])) {
   if (argv[2] === 0 || argv[2] === 1) {
     console.log(1);
   } else {
-	let fact = 1;
-    for (let i = 2 ; i <= argv[2]; i++) {
+    let fact = 1;
+    for (let i = 2; i <= argv[2]; i++) {
       fact *= i;
-	}
+    }
     console.log(fact);
   }
+} else {
+  console.log(1);
 }
