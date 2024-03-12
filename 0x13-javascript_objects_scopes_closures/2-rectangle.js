f35abb16665ded594b99388dfe92a9781454bfb8
lupamo <arnoldlupamo@gmail.com>
2024-03-12 11:03:07 -0700

    rec

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
+    if (typeof w !== 'number' || w <= 0 || !Number.isInteger(w)) {
+        w = 0;
+    }
+    if (typeof h !== 'number' || h <= 0 || !Number.isInteger(h)) {
+        h = 0;
+    }
     this.width = w;
     this.height = h;
-    if (w <= 0 || h <= 0) {
-      return {};
-    }
   }
 }
 module.exports = Rectangle;
