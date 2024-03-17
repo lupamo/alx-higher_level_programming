4b02db1d7b96d32f9f2ed92477eb7ac6cee7f4d1
lupamo <arnoldlupamo@gmail.com>
2024-03-16 19:38:09 -0700

    query

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,6 +3,7 @@ import MySQLdb
 import sys
 
 """A script that lists rows from a sql database"""
+
 db = MySQLdb.connect(
     host="localhost",
     port=3306,
@@ -11,17 +12,22 @@ db = MySQLdb.connect(
     db='hbtn_0e_0_usa')
 
 """creating a cursor object"""
+
 cur = db.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
 
 """fetches rows in the table"""
+
 rows = cur.fetchall()
 
 """display rows in database"""
+
 for row in rows:
     print(row)
 
 
 """closing the cusor and db connection"""
+
 cur.close()
 db.close()
+
