8873ac90dcd7200b2bfa425e7db45af26fbebb7a
lupamo <arnoldlupamo@gmail.com>
2024-03-17 23:54:17 -0700

    city search

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,8 +3,6 @@ if __name__ == '__main__':
     import MySQLdb
     from sys import argv
 
-    username, password, database = argv[1], argv[2], argv[3]
-
     """connecting to a MySQL database"""
     db = MySQLdb.connect(
             host="localhost",
@@ -15,12 +13,11 @@ if __name__ == '__main__':
             )
     cur = db.cursor()
 
-    query_search = "SELECT cities.id, cities.name, states.name \
+    cur.execute("SELECT cities.id, cities.name, states.name \
             FROM cities \
             INNER JOIN states \
             ON cities.state_id = states.id \
-            ORDER BY cities.id ASC"
-    cur.execute(query_search)
+            ORDER BY cities.id ASC")
     query_result = cur.fetchall()
 
     for row in query_result:
