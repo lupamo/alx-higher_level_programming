00add5ff95b0a63f5f07a326e6887b602f326651
lupamo <arnoldlupamo@gmail.com>
2024-03-18 01:14:05 -0700

    class Base

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class State"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """Ïnheiting from Base class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
