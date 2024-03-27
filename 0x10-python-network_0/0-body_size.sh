495c4a5d2c24cb4afaa0fa78f9890cfbfd43d516
lupamo <arnoldlupamo@gmail.com>
2024-03-27 21:19:08 +0300

    three liner

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/bash
-#a script that takes a url sends a request to that url  and displays the size of the body
-
-#check url
-if [ "$#" -ne 1 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-#Fetching URL
-URL="$1"
-
-content=$(curl -s -w "\n%{size_download}\n" "$URL")
-size=${content#* }
-
-echo "$size"
+#a script that takes a url sends a request to that url  and displays the size of the url body
+curl -sI "$1" | grep "content_Length:" | cut -d " " -f 2
