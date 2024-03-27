697a58ccdc54318cb181e21ad7fbe340d6c8a329
lupamo <arnoldlupamo@gmail.com>
2024-03-27 22:30:22 +0300

    peak reverse

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A functon that finds peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
