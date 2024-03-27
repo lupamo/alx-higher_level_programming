143ca57c50e0aa6f66a6fa8fd0191cecd02a85a2
lupamo <arnoldlupamo@gmail.com>
2024-03-27 22:32:50 +0300

    requierment

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,7 @@ A functon that finds peak in a list of unsorted integers
 
 
 def find_peak(list_of_integers):
+    """list integers reversed"""
     if list_of_integers:
         list_of_integers.sort(reverse=True)
         return list_of_integers[0]
