cfa7da056d66b8ca4550b1c47181a8a305bb5c69
lupamo <arnoldlupamo@gmail.com>
2024-03-28 22:07:30 +0300

    changes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,13 +9,13 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email =  sys.argv[2]
-    
+    email = sys.argv[2]
+
     email_data = urllib.parse.urlencode({'email': email})
     email_data = email_data.encode('utf-8')
     request_client = urllib.request.Request(url, data=email_data)
-    
+
     with urllib.request.urlopen(request_client) as response:
         read_content = response.read()
-        
+
         print(read_content.decode('utf-8'))
