721d2f54fdabf61872756c5811b94cf93613036d
lupamo <arnoldlupamo@gmail.com>
2024-03-29 02:19:23 +0300

    body response fetch for url,

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,15 +3,11 @@
 A script that fetches a specific URL
 """
 
-from urllib import request
+import requests
 
 if __name__ == "__main__":
-    url_read = request.Request("https://alx-intranet.hbtn.io/status")
-
-    with request.urlopen(url_read) as resp:
-        body = resp.read()
-        header_set = resp.headers.get_content_charset() or "utf-8"
-        body_decoded = body.decode(header_set)
-        print("Body response:")
-        print("	-type: {}".format(type(body)))
-        print("	-content: {}".format(body))
+    resp = requests.get("https://alx-intranet.hbtn.io/status")
+    body = resp.text
+    print("Body response:")
+    print("	-type: {}".format(type(body)))
+    print("	-content: {}".format(body))
