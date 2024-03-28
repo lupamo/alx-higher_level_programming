92060d66df7c708de27c8c7dcec928a67c338fd3
lupamo <arnoldlupamo@gmail.com>
2024-03-28 21:07:43 +0300

    request Id found.  sys import url

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,13 +8,5 @@ value of X-Request-Id variable in header
 import urllib.request
 import sys
 
-if len(sys.argv) < 1:
-    print("No Url found")
-
-url = sys.argv[1]
-r = urllib.request.urlopen(url)
-for header in r.getheaders():
-    if header[0] == 'X-Request-Id':
-        request_id = header[1]
-        print(request_id)
-        break
+with urllib.request.urlopen(sys.argv[1]) as response:
+    print(response.headers.get('X-Request-Id'))
