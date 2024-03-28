e35e2bdabb74d63c91525c790a3826d3c3390f6a
lupamo <arnoldlupamo@gmail.com>
2024-03-28 22:05:26 +0300

    email respones to server

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,5 +8,6 @@ value of X-Request-Id variable in header
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+script that takes in a email and URl and sends a post request to the passed URL
+"""
+
+import sys
+import urllib.parse
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email =  sys.argv[2]
+    
+    email_data = urllib.parse.urlencode({'email': email})
+    email_data = email_data.encode('utf-8')
+    request_client = urllib.request.Request(url, data=email_data)
+    
+    with urllib.request.urlopen(request_client) as response:
+        read_content = response.read()
+        
+        print(read_content.decode('utf-8'))
