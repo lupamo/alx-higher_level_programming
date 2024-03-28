0af338ab849d52ed617728d8dd58d03a35cf1294
lupamo <arnoldlupamo@gmail.com>
2024-03-28 20:34:09 +0300

    request Id found

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A script that sends request nd displays value of X-Request-Id variable in header"""
+
+
+import urllib.request
+
+from sys import argv
+
+url = argv[1]
+
+r =  urllib.request.urlopen(url)
+for header in r.getheaders():
+	if header[0] == 'X-Request-Id':
+		request_id = header[1]
+		print(request_id)
+		break
