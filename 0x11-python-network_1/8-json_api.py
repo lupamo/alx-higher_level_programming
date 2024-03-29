8ea6e859e7b5ffb6dfac2381a656157a21298f8f
lupamo <arnoldlupamo@gmail.com>
2024-03-29 03:46:19 +0300

    server name search

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A script that takes in a letter and sends a POST
+request to a url
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    v = {"q": q}
+    
+    try:
+        resp = requests.post(url, data=v)
+        data = resp.json()
+
+        if data:
+            print("[{}] {}".format(data.get(id), data.get('name')))
+        else:
+            print("No result")
+    except (ValueError) as ecrr:
+        print("Not a valid JSON")
