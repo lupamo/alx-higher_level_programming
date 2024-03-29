476c31081cce1dcb5be88f442f3de52ae1ab1a52
lupamo <arnoldlupamo@gmail.com>
2024-03-29 03:12:44 +0300

    pycodestyle

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,12 +10,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     resp = requests.get(url)
-    
+
     if resp.status_code >= 400:
         print("Error code: {}".format(resp.status_code))
     else:
         print(resp.text)
-    
-	
-        
-        
