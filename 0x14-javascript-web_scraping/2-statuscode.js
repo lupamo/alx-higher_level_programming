1117db318ff57af3dc919a72cb441508cd3a008e
lupamo <arnoldlupamo@gmail.com>
2024-04-24 02:12:13 +0300

    status code 404 request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,10 +4,10 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request(url, function(error, response, body) {
-   if (error) {
-      console.log(error);
-      return;
-   }
-   console.log(`code: ${response.statusCode}`);
-})
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(`code: ${response.statusCode}`);
+});
