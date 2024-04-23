2a1fe46454a97e5fb03cf395ae36ccf898b62e26
lupamo <arnoldlupamo@gmail.com>
2024-04-24 02:09:41 +0300

    status code 404 request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 /** A script that displays status code of a Request**/
+const request = require('request');
 
 const url = process.argv[2];
 
-fetch(`${url}`)
-  .then(response => {
-    console.log(`code: ${response.status}`);
-  });
+request(url, function(error, response, body) {
+   if (error) {
+      console.log(error);
+      return;
+   }
+   console.log(`code: ${response.statusCode}`);
+})
