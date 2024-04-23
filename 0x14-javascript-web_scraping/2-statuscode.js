a1c9bbdbd2e75b6a7ca5a6abbc7a2d7ba9d1ac91
lupamo <arnoldlupamo@gmail.com>
2024-04-24 01:55:07 +0300

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+/** A script that displays status code of a Request**/
+
+const url = process.argv[2];
+
+fetch(`${url}`)
+  .then(response => {
+    console.log(`code: ${response.status}`);
+  });
