d20c41e5befcfc4633777a8e7e457e8597170a20
lupamo <arnoldlupamo@gmail.com>
2024-05-03 00:30:19 +0300

    one to 5

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+$('#toggle_header').click(function () {
+  const header = $('header');
+  if (header.hasClass('red')) {
+	header.removeClass('red').addClass('green');
+  } else if (header.hasClass('green')) {
+	header.removeClass('green').addClass('red');
+  }
+});
